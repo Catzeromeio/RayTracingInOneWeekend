acf4730e00ef068320fabd73020806929ae116d1
Catzero <catzeromeio@hotmail.com>
2021-03-19 03:56:16 +0800

    feat: set frame rate

--- GameMain.cs
+++ GameMain.cs
@@ -25,25 +25,42 @@ namespace RayTracingInOneWeekend
     public class GameMain
     {
         public static Graphics graphic;
+        public static SolidBrush blackBrush;
         public static Bitmap blitMap;
         public static Vector3[] frameBuffer;
         public static Vector3[] depthBuffer;
+
+        public static double timeSinceStartUp_s;
+        public static double timeStart_s;
+        public static double deltaTime_s;
+        public static double timeLastFrameStart_s;
+        public static float frameRate;
 
 
         public static void GameInit(Form form)
         {
             GameMain.graphic = form.CreateGraphics();
+            blackBrush = new SolidBrush(Color.Black);
             frameBuffer = new Vector3[Screen.Width * Screen.Height];
             depthBuffer = new Vector3[Screen.Width * Screen.Height];
             blitMap = new Bitmap(Screen.Width , Screen.Height);
+
+            timeStart_s = DateTime.Now.Ticks/ TimeSpan.TicksPerSecond;
+            timeSinceStartUp_s = 0;
+            deltaTime_s = 0;
+            timeLastFrameStart_s = 0;
+            frameRate = 60;
         }
 
         public static void GameMainProc()
         {
             while (true)
             {
-                Update();
-                Render();
+                if (TimeCheck())
+                {
+                    Update();
+                    Render();
+                }
             }
         }
 
@@ -54,7 +71,10 @@ namespace RayTracingInOneWeekend
 
         public static void Render()
         {
-            ClearAllBuffers();
+            ClearFrameBuffer();
+            ClearDepthBuffer();
+
+            var scale = DateTime.Now.Millisecond/1000.0f;
 
             // do renderer logic
             for (int i = 0; i < Screen.Width; i++)
@@ -66,17 +86,12 @@ namespace RayTracingInOneWeekend
                     pixel.X = i * 1.0f/Screen.Width;
                     pixel.Y = j * 1.0f/Screen.Height;
                     pixel.Z = 0;
+                    pixel =  pixel * scale;
                     frameBuffer[index] = pixel;
                 }
             }
 
             BlitFrameBufferToScreen();
-        }
-
-        public static void ClearAllBuffers()
-        {
-            ClearFrameBuffer();
-            ClearDepthBuffer();
         }
 
         public static void ClearFrameBuffer()
@@ -95,7 +110,7 @@ namespace RayTracingInOneWeekend
             {
                 depthBuffer[i].X = 0;
                 depthBuffer[i].Y = 0;
-                frameBuffer[i].Z = 0;
+                depthBuffer[i].Z = 0;
             }
         }
 
@@ -114,6 +129,7 @@ namespace RayTracingInOneWeekend
             }
 
             graphic.DrawImage(blitMap,0,0);
+            graphic.DrawString(deltaTime_s.ToString() + " " + timeLastFrameStart_s.ToString() + " " + (DateTime.Now.Ticks/TimeSpan.TicksPerSecond).ToString() ,new Font("Arial", 16), blackBrush,0,0);
         }
 
         public static int GetIndex(int x, int y)
@@ -126,6 +142,20 @@ namespace RayTracingInOneWeekend
             int x = index - Screen.Width * y;
 
             return new Coord(x,y);
+        }
+
+        public static bool TimeCheck()
+        {
+            var now = DateTime.Now.Ticks / (double)TimeSpan.TicksPerSecond ;
+            timeSinceStartUp_s =  now - timeStart_s;
+            deltaTime_s = now - timeLastFrameStart_s;
+            if (deltaTime_s  > 1.0/frameRate)
+            {
+                timeLastFrameStart_s = now;
+                return true;
+            }
+
+            return false;
         }
     }
 }
