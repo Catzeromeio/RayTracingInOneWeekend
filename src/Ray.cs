7e0275a563feab434cfd99cae37bc988b7223621
Catzero <catzeromeio@hotmail.com>
2021-03-20 17:37:09 +0800

    feat：add ray

---
+++ src/Ray.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+namespace RayTracingInOneWeekend.src
+{
+    class Ray
+    {
+        public Vector3 Origin { get; set; }
+        public Vector3 direction;
+        public Vector3 Direction {
+            get { return direction; }
+            set
+            {
+                direction = Vector3.Normalize(value);
+            }
+        }
+
+        public Ray(Vector3 origin, Vector3 dir)
+        {
+            Origin = origin;
+            direction = Vector3.Normalize(dir);
+        }
+
+        public Vector3 At(float t)
+        {
+            return Origin + t * direction;
+        }
+    }
+}
