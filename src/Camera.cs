f432011b0258661d6cd1dbd3303ce5b88d27d0a1
Catzero <catzeromeio@hotmail.com>
2021-03-21 18:04:51 +0800

    feat: add msaa

--- src/Camera.cs
+++ src/Camera.cs
@@ -13,6 +13,8 @@ namespace RayTracingInOneWeekend
         private float viewPortHeight;
         private Size resolution;
 
+        public int sampleRoot = 8;
+
         public Size Resolution
         {
             get { return resolution; }
@@ -47,22 +49,35 @@ namespace RayTracingInOneWeekend
         public void Render(HittableList scene)
         {
             //ClearDepthBuffer();
-            //ClearFrameBuffer();
+            ClearFrameBuffer();
 
             for (int i = 0; i < resolution.Width; i++)
             {
                 for (int j = 0; j < resolution.Height; j++)
                 {
                     var index = GetIndex(i, j);
-                    //计算像素中心
-                    var pixelP = new Vector2(i + 0.5f, j + 0.5f);
-                    //中心平移到原点(2D)
-                    pixelP = pixelP - new Vector2(resolution.Width / 2.0f, resolution.Height / 2.0f);
-                    //缩放到视口大小
-                    pixelP = viewPortHeight / resolution.Height * pixelP;
-                    //从眼睛位置到3D空间缩放后像素发射射线
-                    var ray = new Ray(position, new Vector3(pixelP.X, pixelP.Y, position.Z -focalLength) - position);
-                    frameBuffer[index] = GameMain.Ray_Color(ray, scene);
+                    var ss = 1.0f / sampleRoot;
+                    var ss_half = ss / 2.0f;
+
+                    //将像素分割成sampleRoot * sampleRoot个像素进行采样
+                    for (int si = 0; si < sampleRoot; si++)
+                    {
+                        for (int sj = 0; sj < sampleRoot; sj++)
+                        {
+                            //计算像素中心
+                            var pixelP = new Vector2(i + si *ss + ss_half , j + sj*ss + ss_half);
+                            //中心平移到原点(2D)
+                            pixelP = pixelP - new Vector2(resolution.Width / 2.0f, resolution.Height / 2.0f);
+                            //缩放到视口大小
+                            pixelP = viewPortHeight / resolution.Height * pixelP;
+                            //从眼睛位置到3D空间缩放后像素发射射线
+                            var ray = new Ray(position, new Vector3(pixelP.X, pixelP.Y, position.Z -focalLength) - position);
+                            //叠加
+                            frameBuffer[index] += GameMain.Ray_Color(ray, scene);
+                        }
+                    }
+
+                    frameBuffer[index] = 1.0f/(sampleRoot * sampleRoot) * frameBuffer[index];
                 }
             }
         }
